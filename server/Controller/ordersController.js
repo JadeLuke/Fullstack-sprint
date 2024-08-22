e425480386b2eb8921d4b0338e66a9f489d08567
JadeLuke <jadeluke.booysen@gmail.com>
2024-08-22 09:05:31 +0200

    added edit function

--- server/Controller/ordersController.js
+++ server/Controller/ordersController.js
@@ -22,52 +22,51 @@ exports.createAllOrders = (req, res) =>{
         res.send('sent')
     }
     })
-    // .then(data => res.send("sent",data[0]))
-    
-    // .catch(err => console.log(err))
     }
 
 exports.getAllOrders = (req,res) =>{
 
 connection.promise().query('SELECT * FROM Orders') 
-// if(err){
-//     console.log(err);
-// }
-// else{
-//     res.send(result)
-//     console.log(result);
-// }
 .then(data => res.send(data[0]))
 .catch(err => console.log(err))
 
 }
 
-exports.getAllOrdersById = (req,res) => {
-const orders = req.params.id
+exports.getAllOrdersById = (req, res) => {
+    const orderId = parseInt(req.params.id, 10);
 
-connection.query('SELECT * FROM Orders WHERE OrderId = ?', orders, (err,result) =>{
-if(err){
-    console.log(err)
-}
-else{
-    res.send(result)
-}
-})
-}
+    if (isNaN(orderId)) {
+        return res.status(400).send('Invalid order ID');
+    }
 
-exports.updateAllOrdersById = (req,res) =>{
-const orderId = req.params.id
-const { customerName, meal, takeAway, price} = req.body
+    connection.query('SELECT * FROM Orders WHERE OrderId = ?', [orderId], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error fetching order');
+        }
+        if (results.length === 0) {
+            return res.status(404).send('Order not found');
+        }
+        res.json(results[0]);
+    });
+};
 
-connection.query('UPDATE Orders SET meal = ? WHERE orderId = ?', [meal,orderId], (err)=>{
-if(err){
-    console.log(err);
-}
-else{
-    res.send('updated')
-}
-})
-}
+exports.updateAllOrdersById = (req, res) => {
+    const orderId = parseInt(req.params.id, 10);
+    const { customerName, meal, takeAway, price } = req.body;
+
+    if (isNaN(orderId) || !customerName || !meal || !takeAway || isNaN(price)) {
+        return res.status(400).send('Invalid input');
+    }
+
+    connection.query('UPDATE Orders SET meal = ?, price = ?, takeAway = ? WHERE OrderId = ?', [meal, price, takeAway, orderId], (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error updating order');
+        }
+        res.send('Order updated');
+    });
+};
 
 exports.deleteAllOrdersById = (req,res) =>{
 const orderId = req.params.id
